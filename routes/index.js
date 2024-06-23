140f9bc2507b6578afefe559f7aff094d32f578e
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2024-06-22 20:16:21 -0700

    feat: set ujp server to recieve requests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
+const app = express();
+
+app.use(express.static(path.resolve("C:/Code/smart-home-solutions/smart-home-solutions/client/build/index.html")));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mahaffey' });
-});
+// router.get('/*', function (req, res) {
+//   res.json({index: "test"})
+// });
 
 module.exports = router;
