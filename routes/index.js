32246a21a53600319f57212fba22f032085242f4
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2024-06-26 12:16:16 -0700

    feat: create end points for power toggle

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path')
-const app = express();
+const { Client } = require('tplink-smarthome-api');
 
-app.use(express.static(path.resolve("C:/Code/smart-home-solutions/smart-home-solutions/client/build/index.html")));
+const client = new Client();
+
+client.startDiscovery().on('device-new', (device) => {
+    // device.getSysInfo().then(console.log);
+    console.log(device)
+});
 
 /* GET home page. */
-// router.get('/*', function (req, res) {
-//   res.json({index: "test"})
-// });
+router.get('/office', function (req, res) {
+    const device = client.devices.get("80066D894D81FAAF8F5B674CACA3AF57222ED2D2");
+    device.togglePowerState()
+    device.getPowerState().then(value => {
+        res.json({bedroom: !value})
+    })
+});
+router.get('/bedroom', function (req, res) {
+    const device = client.devices.get("80065F1D7A442005356A69006A2A51EC222ED7AC")
+    device.togglePowerState()
+    device.getPowerState().then(value => {
+        res.json({bedroom: !value})
+    })
+    
+});
 
 module.exports = router;
