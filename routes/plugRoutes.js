8cc9a576a72493e7b193a4ef6eaec240da7fde78
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2024-06-28 21:26:37 -0700

    feat error handling, snackbar init, button glow

--- routes/plugRoutes.js
+++ routes/plugRoutes.js
@@ -4,27 +4,14 @@ var {toggleDevice} = require("../business/devices")
 
 
 router.get('/:id', async function (req, res) {
-    var {id} = req.params;
-    var success = await toggleDevice(id)
-    console.log(success)
-    res.json({success })
+    const {id} = req.params;
+    try {
+        const success = await toggleDevice(id)
+        res.json({ success })
+    } catch (error) {
+        console.error("Error: " + error)
+        res.status(500).send(error.message)
+    }
 })
-
-/* GET home page. */
-// router.get('/office', function (req, res) {
-//     const device = client.devices.get("80066D894D81FAAF8F5B674CACA3AF57222ED2D2");
-//     device.togglePowerState()
-//     device.getPowerState().then(value => {
-//         res.json({bedroom: !value})
-//     })
-// });
-// router.get('/bedroom', function (req, res) {
-//     const device = client.devices.get("80065F1D7A442005356A69006A2A51EC222ED7AC")
-//     device.togglePowerState()
-//     device.getPowerState().then(value => {
-//         res.json({bedroom: !value})
-//     })
-    
-// });
 
 module.exports = router;
